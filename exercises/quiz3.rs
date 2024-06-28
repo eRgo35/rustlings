2e95ce850396e615b54ddf96adbf609882f8ca2f
Michael Czyż <mike@c2yz.com>
2024-06-28 15:56:59 +0200

    lifetimes, tests, generics

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,47 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub trait ReportCard {
+    fn print(&self) -> String;
+}
 
-pub struct ReportCard {
+pub struct NumericalReportCard {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+pub struct AlphabeticalReportCard {
+    pub grade: f32,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl ReportCard for NumericalReportCard {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+impl ReportCard for AlphabeticalReportCard {
+    fn print(&self) -> String {
+        let grade = match &self.grade {
+            1.0 => "F-",
+            1.1..=1.5 => "F",
+            1.6..=2.0 => "F+",
+            2.1..=2.5 => "D-",
+            2.6..=3.0 => "D",
+            3.1..=3.5 => "C",
+            3.6..=4.0 => "B",
+            4.1..=4.5 => "A-",
+            4.6..=5.0 => "A",
+            5.1..=5.5 => "A+",
+            _ => "Invalid Grade"
+        };
+
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, grade)
     }
 }
 
@@ -37,7 +66,7 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
+        let report_card = NumericalReportCard {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
@@ -51,8 +80,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = AlphabeticalReportCard {
+            grade: 5.5,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
